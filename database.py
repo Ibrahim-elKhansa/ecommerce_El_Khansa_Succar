a8d14f1693c8d4c992058242519d15a16f78990d
Omar Succar <omarsuccar02@gmail.com>
2024-12-02 01:20:22 +0200

    db documentation

--- database.py
+++ database.py
@@ -4,6 +4,28 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./database.db"
 
+"""
+Module: database
+
+This module configures and initializes the database connection for the application.
+It defines the SQLAlchemy engine, session, and declarative base for database models.
+
+Configuration:
+    - DATABASE_URL: Specifies the path to the SQLite database.
+    - `engine`: SQLAlchemy engine for connecting to the SQLite database.
+    - `SessionLocal`: Configured sessionmaker instance for database sessions.
+    - `Base`: Declarative base class for defining database models.
+
+Functions:
+    - get_db(): Provides a database session for FastAPI dependency injection.
+      Ensures proper session lifecycle management (creation and closure).
+
+Usage:
+    - Import `Base` to define database models in the application.
+    - Use `get_db` in FastAPI routes to interact with the database.
+"""
+
+
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
